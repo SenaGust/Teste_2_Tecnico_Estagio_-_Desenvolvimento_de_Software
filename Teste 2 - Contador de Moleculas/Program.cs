8081e4a1369f5296a9a9344065d586bfd57eded6
Gustavo Sena <gustavosena98@LIVE.COM>
2019-03-05 22:01:27 -0300

    Criação da Classe Formula (Inclui métodos e atributos)

--- Teste 2 - Contador de Moleculas/Program.cs
+++ Teste 2 - Contador de Moleculas/Program.cs
@@ -6,6 +6,41 @@ using System.Threading.Tasks;
 
 namespace Teste_2___Contador_de_Moleculas
 {
+    class Formula
+    {
+        #region Atributos
+        private string elementoQuimico;
+        private int quantidade;
+        #endregion
+
+        #region Métodos Getters e Setters
+        public string ElementoQuimico
+        {
+            get { return elementoQuimico; }
+            set { elementoQuimico = value; }
+        }
+        public int Quantidade
+        {
+            get { return quantidade; }
+            set { quantidade = value; }
+        }
+        #endregion
+
+        #region Contrutor
+        public Formula(string elemento)
+        {
+            elementoQuimico = elemento;
+            quantidade = 0;
+        }
+        #endregion
+
+        #region Métodos
+        public override string ToString()
+        {
+            return elementoQuimico + ": " + quantidade + ".";
+        }
+        #endregion
+    }
     class Program
     {
         static void Main(string[] args)
